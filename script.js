e1196316aab7336d1729c9e20e05c73b161063a5
Katie Fritz <katie@katiemfritz.com>
2017-02-05 21:35:01 -0500

    localstorage stores theme choice. code could probably be refactored

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 var toggleButton = document.getElementById('toggle-theme')
 var currentClass = document.body.getAttribute('class')
+var theme = ''
 
-var toggleTheme = function () {
-  if (currentClass.includes('night')) {
+var loadTheme = function () {
+  theme = window.localStorage.getItem('theme')
+  if (theme !== null) {
+    document.body.setAttribute('class', theme)
+  } else {
     document.body.setAttribute('class', 'day-theme')
     currentClass = 'day-theme'
-  } else if (currentClass.includes('day')) {
-    document.body.setAttribute('class', 'night-theme')
-    currentClass = 'night-theme'
   }
 }
 
+var toggleTheme = function () {
+  if (currentClass.includes('day')) {
+    theme = 'night-theme'
+    currentClass = 'night-theme'
+  } else {
+    theme = 'day-theme'
+    currentClass = 'day-theme'
+  }
+  window.localStorage.setItem('theme', theme)
+  document.body.setAttribute('class', currentClass)
+}
+
+loadTheme()
 toggleButton.addEventListener('click', toggleTheme)
