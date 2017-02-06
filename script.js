383afce555269ce91449251bbb492efa8a6def9c
Katie Fritz <katie@katiemfritz.com>
2017-02-06 10:19:38 -0500

    refactor theme stuff and add comments

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 var toggleButton = document.getElementById('toggle-theme')
-var currentClass = document.body.getAttribute('class')
 var theme = ''
 
 var loadTheme = function () {
+  // Get the stored value for theme
   theme = window.localStorage.getItem('theme')
+  // If theme is set, use that theme
   if (theme !== null) {
     document.body.setAttribute('class', theme)
+  // Otherwise, start out with day-theme
   } else {
     document.body.setAttribute('class', 'day-theme')
-    currentClass = 'day-theme'
+    theme = 'day-theme'
   }
 }
 
 var toggleTheme = function () {
-  if (currentClass.includes('day')) {
+  // If theme is day-theme, set it to night-theme
+  if (theme === 'day-theme') {
     theme = 'night-theme'
-    currentClass = 'night-theme'
+  // Otherwise, assume it's night-theme, and set it to day-theme
   } else {
     theme = 'day-theme'
-    currentClass = 'day-theme'
   }
+  // Store the new theme and apply the theme to the site
   window.localStorage.setItem('theme', theme)
-  document.body.setAttribute('class', currentClass)
+  document.body.setAttribute('class', theme)
 }
 
+// Make sure there's a theme when you load the page
 loadTheme()
+// Initiate toggle button 
 toggleButton.addEventListener('click', toggleTheme)
